b362040c9de621a3566ba9c23e3fb80b68f2cfe9
Vishal <vishalvijayjain@outlook.com>
2021-10-30 18:18:04 +0530

    initial commit

---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const router = express.Router();
+const { check, validationResult } =require('express-validator/check');
+
+ 
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+   check('email', 'Please add a valid email').isEmail(),
+   check('password', 
+   'Please enter a password with 6 or more characters').isLength({min :6}) 
+], 
+(req,res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    res.send('Users route');
+});
+ module.exports =router;
+ 
